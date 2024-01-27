98af9f198bd991e427589842d14e4359e4aa4470
winmoh <mohamedaouinti2023@gmail.com>
2024-01-27 14:12:34 +0100

    pipeline and model integrated

--- src/main/java/com/example/medinsighthub/model/modelResponse.java
+++ src/main/java/com/example/medinsighthub/model/modelResponse.java
@@ -3,12 +3,14 @@ package com.example.medinsighthub.model;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class modelResponse {
     private String class_name;
-    private String confidence_score;
+    private float confidence_score;
 
 }
